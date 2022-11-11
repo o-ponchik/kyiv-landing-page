134152a66c6d2f51c14d9ac222342ad52c6adf16
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-11-12 00:29:51 +0200

    add_animation

---
+++ index.js
@@ -0,0 +1,17 @@
+function revealElement() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElement);
